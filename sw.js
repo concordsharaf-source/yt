fe9593349d3278b03fc5492cb6ddc63ad810a019
concordsharaf-source <concordsharaf@gmail.com>
2026-08-05 00:16:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (event) => {
+  console.log('تم تثبيت Service Worker بنجاح');
+});
+
+self.addEventListener('fetch', (event) => {
+  // هنا تتم إدارة طلبات الشبكة (يمكنك لاحقاً إضافة أكواد التخزين المؤقت هنا)
+});
 
